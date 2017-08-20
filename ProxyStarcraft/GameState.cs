afe4e9329467853704cb8b1db7e02e5386823ef4
Timothy Bond <tmbond@gmail.com>
2017-08-20 13:05:25 -0400

    Moving Proto definitions into core library.

---
+++ ProxyStarcraft/GameState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using ProxyStarcraft.Proto;
+
+namespace ProxyStarcraft
+{
+    public class GameState
+    {
+        public GameState(ResponseGameInfo gameInfo, Observation observation, Dictionary<uint, UnitTypeData> unitTypes)
+        {
+            this.GameInfo = gameInfo;
+            this.Observation = observation;
+            this.UnitTypes = unitTypes;
+        }
+
+        public ResponseGameInfo GameInfo { get; private set; }
+
+        public Observation Observation { get; private set; }
+
+        public IReadOnlyDictionary<uint, UnitTypeData> UnitTypes { get; private set; }
+    }
+}
---
+++ ProxyStarcraft/Proto/README.md
@@ -0,0 +1,2 @@
+﻿This library is generated code, made from the protocol buffer descriptions in
+https://github.com/Blizzard/s2client-proto. It should not be edited.
