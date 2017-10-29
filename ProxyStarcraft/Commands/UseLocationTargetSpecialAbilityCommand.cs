c753b519ec2caf6914fee805bcd04147515771bc
Dan Tarjeft <dtarjeft@gmail.com>
2017-10-29 17:58:18 -0400

    Added support for Special Abilities that target a location.

---
+++ ProxyStarcraft/Commands/UseLocationTargetSpecialAbilityCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace ProxyStarcraft.Commands
+{
+    public class UseLocationTargetSpecialAbilityCommand : LocationTargetCommand
+    {
+        public SpecialAbilityType Ability { get; set; }
+        public UseLocationTargetSpecialAbilityCommand(Unit unit, float x, float y, SpecialAbilityType ability) : base(unit, x, y)
+        {
+            this.Ability = ability;
+        }
+    }
+}
