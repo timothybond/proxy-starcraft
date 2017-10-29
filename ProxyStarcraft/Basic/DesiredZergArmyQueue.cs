42dbbdaf132fedb074e76a0ba83d06101faec6c1
Dan Tarjeft <dtarjeft@gmail.com>
2017-10-29 17:56:47 -0400

    Added a ZergArmyQueue, and made DesiredArmyQueue generic.

---
+++ ProxyStarcraft/Basic/DesiredZergArmyQueue.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProxyStarcraft.Basic
+{
+    public class DesiredZergArmyQueue : DesiredArmyQueue<ZergUnitType>
+    {
+        protected override Dictionary<ZergUnitType, int> GetUnitsByType(GameState gameState) => gameState.Units.OfType<ZergUnit>().GroupBy(t => t.ZergUnitType).ToDictionary(group => group.Key, g => g.Count());
+    }
+}
